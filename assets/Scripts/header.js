2816cfd54e1905b3bff36d49ed4d7f109ebbd6c9
hjude-design <harrison@hjude.com>
2023-04-05 14:24:49 -0400

    first commit

---
+++ assets/Scripts/header.js
@@ -0,0 +1,3 @@
+<script>
+    alert('hello this is js working properly');
+</script>
---
+++ patterns/basic-cover.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Title: basic-cover
+ * Slug: hjude_wp/basic-cover
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:cover {"url":"<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/josh-wilburne-rdJXlCdRVaQ-unsplash.jpg","id":25,"dimRatio":50,"isDark":false,"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover is-light"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><img class="wp-block-cover__image-background wp-image-25" alt="" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/josh-wilburne-rdJXlCdRVaQ-unsplash.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","placeholder":"Write title…","style":{"typography":{"fontStyle":"italic","fontWeight":"900"}},"textColor":"white","fontSize":"large","fontFamily":"silverknife-italic"} -->
+<p class="has-text-align-center has-white-color has-text-color has-silverknife-italic-font-family has-large-font-size" style="font-style:italic;font-weight:900">Oh Hey There, Im Harrison.</p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:cover -->
