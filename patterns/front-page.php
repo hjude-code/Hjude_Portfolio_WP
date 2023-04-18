200bc1fc553b595b3d67fab2898882da429f2d60
hjude-design <harrison@hjude.com>
2023-04-17 20:53:38 -0400

    removed column spacing

---
+++ patterns/front-page.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Title: front-page
+ * Slug: hjude_wp/front-page
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+<!-- wp:create-block/sidebarnav -->
+<!-- wp:navigation {"ref":82,"textColor":"white","showSubmenuIcon":false,"overlayMenu":"never","overlayBackgroundColor":"black","overlayTextColor":"white","layout":{"type":"flex","orientation":"vertical","justifyContent":"right"},"style":{"spacing":{"blockGap":"0"}}} /-->
+<!-- /wp:create-block/sidebarnav -->
+
+<!-- wp:create-block/messageheader {"greeting":"Wowza its some text aint that coolio?!","backgroundColor":"black","textColor":"white"} -->
+<!-- wp:image -->
+<figure class="wp-block-image"><img alt=""/></figure>
+<!-- /wp:image -->
+<!-- /wp:create-block/messageheader -->
+
+<!-- wp:columns {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
+<div class="wp-block-columns" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":26,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/anthony-rosset-4P5zGd7UyuU-unsplash.jpg" alt="" class="wp-image-26"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"50vw","style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"}},"border":{"top":{"style":"none","width":"0px"},"right":{"style":"none","width":"0px"},"bottom":{"style":"none","width":"0px"},"left":{"style":"none","width":"0px"}}}} -->
+<div class="wp-block-column" style="border-top-style:none;border-top-width:0px;border-right-style:none;border-right-width:0px;border-bottom-style:none;border-bottom-width:0px;border-left-style:none;border-left-width:0px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px;flex-basis:50vw"></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
