d3b70155600877faabcf1f76feb4e1fcdb425da4
hjude-design <harrison@hjude.com>
2023-05-03 08:35:52 -0400

    removed extranious characters that caused an error

--- functions.php
+++ functions.php
@@ -5,6 +5,6 @@ function register_blocks()
     register_block_type( dirname(__FILE__) . '/src/blocks/imagenav/build/block.json' );
     register_block_type( dirname(__FILE__) . '/src/blocks/imagenavitem/build/block.json' );
     register_block_type( dirname(__FILE__) . '/src/blocks/messageheader/build/block.json' );
-    register_block_type( dirname(__FILE__) . '/src/blocks/sideBarNav/build/block.json' );cl
+    register_block_type( dirname(__FILE__) . '/src/blocks/sideBarNav/build/block.json' );
 }
 add_action('init', 'register_blocks');
