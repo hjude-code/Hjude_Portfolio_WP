d72dca2551ec3b31551fb7456ea327c1d6f24204
hjude-design <harrison@hjude.com>
2023-05-21 10:03:09 -0400

    misc header updates

--- src/blocks/messageheader/build/index.asset.php
+++ src/blocks/messageheader/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'efd84f8c3e4f8f97f50c');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd43cc4f37b7c743c515f');
