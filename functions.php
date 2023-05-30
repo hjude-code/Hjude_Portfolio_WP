3ac565935f2ff44bf8ff128671db1479542cbbc3
hjude-design <harrison@hjude.com>
2023-05-29 20:47:54 -0400

    registered li-group block

--- functions.php
+++ functions.php
@@ -21,7 +21,7 @@ function register_blocks()
     register_block_type( dirname(__FILE__) . '/src/blocks/imagenavitem/build/block.json' );
     register_block_type( dirname(__FILE__) . '/src/blocks/messageHeader/build/block.json' );
     register_block_type( dirname(__FILE__) . '/src/blocks/sidebarnav/build/block.json' );
-
+    register_block_type( dirname(__FILE__) . '/src/blocks/li-group/build/block.json' );
     register_block_type( dirname(__FILE__) . '/src/blocks/spinWheel/build/block.json' );
 
     register_block_type( dirname(__FILE__) . '/src/blocks/skills/build/block.json' );
