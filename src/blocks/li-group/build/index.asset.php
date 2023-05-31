16ed40018b5b21f168524d54a0c083775a7010b6
hjude-design <harrison@hjude.com>
2023-05-31 08:26:05 -0400

    Updated li-group block to have accent color border on front end

--- src/blocks/li-group/build/index.asset.php
+++ src/blocks/li-group/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2440dcb5cc06ebbc4472');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a425bf509f2ec8edb01e');
--- src/blocks/li-group/src/render.php
+++ src/blocks/li-group/src/render.php
@@ -2,8 +2,16 @@
 
 $accent = $attributes['Accent'];
 
+$border_style = 'border-left:0.5rem solid ' . $accent . '!important';
+
+$wrapper_attributes = get_block_wrapper_attributes(
+	[
+		'style' => $border_style
+	]		
+);	
+
 ?>
-<div <?php echo get_block_wrapper_attributes(); ?>>
+<div <?php echo $wrapper_attributes; ?>>
 	<p><?php echo $attributes['Handle'] ?></p>
 	<p><span style="color:<?php echo $accent ?>">on</span><?php echo $attributes['Platform'] ?></p>
 </div>
