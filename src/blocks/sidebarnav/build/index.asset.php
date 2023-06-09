6b3a6cc5b446a8fcb4b361265cddfd199da7a459
hjude-design <harrison@hjude.com>
2023-06-09 17:39:31 -0400

    begin rebuild of nav css

--- src/blocks/sidebarnav/build/index.asset.php
+++ src/blocks/sidebarnav/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b3f70a01c613cac8b86a');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5594914340b3f270d413');
