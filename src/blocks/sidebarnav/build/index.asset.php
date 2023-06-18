499c5959c1588d35b50899bd0c23df913d63d3fa
hjude-design <harrison@hjude.com>
2023-06-18 12:36:05 -0400

    editor bugs updated for navigation

--- src/blocks/sidebarnav/build/index.asset.php
+++ src/blocks/sidebarnav/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5594914340b3f270d413');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f35c906eed3334f10852');
